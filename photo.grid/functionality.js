83465742a78a82e28585203903fec08db08d3af7
Andrei <andrei.bast@yahoo.com>
2020-05-21 20:21:18 +0300

    part4done

--- photo.grid/functionality.js
+++ photo.grid/functionality.js
@@ -1,23 +1,20 @@
 $(document).ready(function(){ 
-    
     $(".category_item").click(function(){
-        $("div[style*=none]").css({'display' : ''});
         var category = $(this).attr("id");
-        
         if(category == "all"){
             $(".clothes_item").addClass("hide"); 
+            
             setTimeout(function(){ 
                 $(".clothes_item").removeClass("hide");
-            }, 300);
+            }, 1000);
                
         }else{
             $(".clothes_item").addClass("hide");
-            
             setTimeout(function(){ 
                 $("." + category).removeClass("hide");
-                $("div .hide").css("display", "none"); //this is working
-            }, 300);
-            
-        }
+                $("div[style*=none]").css({'display' : ''}); 
+                $("div .hide").css("display", "none"); 
+            }, 1000);
+        }   
     })
 })
